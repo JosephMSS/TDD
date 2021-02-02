cf400c572b3a1487b5b077807f3883d8d6cc0a75
JosephMMS <josephzx1@hotmail.com>
2021-02-01 23:35:50 -0600

    Accessors y Mutators

---
+++ TDD/app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    /**
+     * Mutators
+     */
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name']=strtolower($name);
+    }
+    /**
+     * Accessors
+     */
+    public function getSlugAttribute()
+    {
+        return str_replace(' ','-',$this->name);
+    }
+}
---
+++ TDD/tests/Feature/Models/PostTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Post;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_set_name_in_lowercase()
+    {
+        $post = new Post();
+        $post->name = 'Proyecto en php';
+        $this->assertEquals('proyecto en php', $post->name);
+    }
+    public function test_get_slug()
+    {
+        $post = new Post();
+        $post->name = 'Proyecto en php';
+        $this->assertEquals('proyecto-en-php', $post->slug);
+    }
+}
